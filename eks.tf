cbb5a3296187b8f2478dca8d6e144211517f62df
jyotiparmar20 <jparmarjyoti20@gmail.com>
2026-02-26 16:30:21 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -167,7 +167,7 @@ resource "aws_eks_node_group" "node_group" {
     min_size     = 1
   }
 
-  instance_types = ["t2.micro"]
+  instance_types = ["t2.medium"]
 
   depends_on = [
     aws_eks_cluster.eks_cluster
